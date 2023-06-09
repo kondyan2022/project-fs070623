4ccd8b7f985f2a188536600a14b4a9aa825a7dc7
kondyan2022 <119456733+kondyan2022@users.noreply.github.com>
2023-06-09 08:03:45 +0000

    Deploying to gh-pages from @ kondyan2022/project-fs070623@1118677e885cc6f1310bb5ac1acaa8a937a6f286 🚀

--- catalog.00021a0e.js
+++
@@ -1,2 +0,0 @@
-const t={switch:document.querySelector(".toggle-switch"),totop:document.querySelector(".totop")};new IntersectionObserver((e,o)=>{e.forEach(e=>{e.target===t.switch&&(t.totop.style.display=e.isIntersecting?"none":"inline-block")})},{root:null,rootMargin:"50px",threshold:1}).observe(t.switch);
-//# sourceMappingURL=catalog.00021a0e.js.map
