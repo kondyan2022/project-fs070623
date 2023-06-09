f624690f538c37aff5541047b28a31360f9dc71c
Natalia-Havashi <natali.gavashi@gmail.com>
2023-06-09 19:14:48 +0300

    search

---
+++ src/js/search.js
@@ -0,0 +1,36 @@
+import Pagination from 'tui-pagination';
+const container = document.getElementById('pagination');
+
+
+
+
+const options = { // below default value of options
+     totalItems: 10,
+     itemsPerPage: 20,
+     visiblePages: 10,
+     page: 1,
+     centerAlign: false,
+     firstItemClassName: 'tui-first-child',
+     lastItemClassName: 'tui-last-child',
+     template: {
+         page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+         currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+         moveButton:
+             '<a href="#" class="tui-page-btn tui-{{type}}">' +
+                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
+             '</a>',
+         disabledMoveButton:
+             '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+                 '<span class="tui-ico-{{type}}">{{type}}</span>' +
+             '</span>',
+         moreButton:
+             '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+                 '<span class="tui-ico-ellip">...</span>' +
+             '</a>'
+     }
+};
+const pagination = new Pagination(container, options);
+
+console.log('pagination:', pagination)
+
+
