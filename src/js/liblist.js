5dfbb556844f8cef0713a4b36784d2633bdb4be2
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-06-10 22:24:59 +0200

    +

--- src/js/liblist.js
+++ src/js/liblist.js
@@ -1,17 +1,23 @@
-// import saveToLocalStorage from './local-storage';
-// import loadFromLocalStorage from './local-storage';
-
-// import removeFromLocalStorage from './local-storage';
-// import addListLibrary from './local-storage';
-// import moviesDataUpdate from './local-storage';
+// import TMDBApiService from './tmdb-api'
 import getGenres from "./get-genres";
-
+import getFilmCard from './film-card';
+import {
+    saveToLibrary,
+    getLibraryList,
+    isInLibrary,
+    removeFromLibrary,
+} from './local-storage';
 
 const arrayGenres = getGenres();
 console.log(arrayGenres)
 
 const listCards = document.querySelector('.js-liblist');
 const selectWrapper = document.querySelector('#library-filter');
+
+
+// function renderMovieCards() {
+// }
+
 
 function createMarkupInSelect(item) {
     return `
@@ -23,10 +29,3 @@ function renderMarkupInSelect() {
     const gotGenre = arrayGenres.map(genre => createMarkupInSelect(genre)).join('');
     selectWrapper.innerHTML = gotGenre;
 }
-
-renderMarkupInSelect()
-
-function renderMovieCards() {
-
-}
-
