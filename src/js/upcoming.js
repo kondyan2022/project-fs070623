aff634a7efe2d01f4296dd6550f6cfa7a1fe5251
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-06-10 17:07:31 +0200

    add some styles

--- src/js/upcoming.js
+++ src/js/upcoming.js
@@ -35,42 +35,44 @@ serviceUpcoming
 function creatMarkup(movie) {
     return `
     <div class="upcoming-left-wrap">
-        <img 
-            srcset="
-                https:/image.tmdb.org/t/p/w342/${movie.poster_path} 342w,
-                https:/image.tmdb.org/t/p/w500/${movie.backdrop_path} 500w,
-                https:/image.tmdb.org/t/p/w780/${movie.backdrop_path} 780w,
-                https:/image.tmdb.org/t/p/original/${movie.backdrop_path} 2000w
-            "
-            sizes="(min-width: 320px),
-                (min-width: 768px),
-                (min-width: 1280px),
-            "
-            src="https:/image.tmdb.org/t/p/original/${movie.backdrop_path}" 
-            alt="${movie.title}"
-            class="upcoming-image"
-            width="805"
-            height="458"
-        />
+        <div class="up-thumb">
+            <img 
+                srcset="
+                    https:/image.tmdb.org/t/p/w342/${movie.poster_path} 342w,
+                    https:/image.tmdb.org/t/p/w500/${movie.backdrop_path} 500w,
+                    https:/image.tmdb.org/t/p/w780/${movie.backdrop_path} 780w,
+                    https:/image.tmdb.org/t/p/original/${movie.backdrop_path} 2000w
+                "
+                sizes="(min-width: 320px),
+                    (min-width: 768px),
+                    (min-width: 1280px),
+                "
+                src="https:/image.tmdb.org/t/p/original/${movie.backdrop_path}" 
+                alt="${movie.title}"
+                class="upcoming-image"
+            />
+        </div>
     </div>
     <div class="upcoming-right-wrap">
         <h2 class="upcoming-movie-title">${movie.title}</h2>
         <table class="upcoming-table">
             <tr class="upcoming-tab-row">
                 <td class="upcoming-data">Release date</td>
-                <td class="upcoming-data">${movie.release_date}</td>
+                <td class="upcom-data-release">${movie.release_date}</td>
             </tr>
             <tr class="upcoming-tab-row">
                 <td class="upcoming-data">Vote / Votes</td>
-                <td class="upcoming-data">${movie.vote_average}/${movie.vote_count}</td>
+                <td class="upcoming-data-average">
+                    <span class="up-average">${movie.vote_average}</span>/<span class="up-count">${movie.vote_count}</span>
+                </td>
             </tr>
             <tr class="upcoming-tab-row">
                 <td class="upcoming-data">Popularity</td>
-                <td class="upcoming-data">${movie.popularity}</td>
+                <td class="upcoming-data-get">${movie.popularity.toFixed(1)}</td>
             </tr>
             <tr class="upcoming-tab-row">
                 <td class="upcoming-data">Genre</td>
-                <td class="upcoming-data">${getGenres()
+                <td class="upcoming-data-get">${getGenres()
             .filter(({ id }) => movie.genre_ids.includes(id))
             .map(({ name }) => name)
             .slice(0, 2)
@@ -79,7 +81,7 @@ function creatMarkup(movie) {
         </table>
         <p class="upcoming-about">About</p>
         <p class="upcoming-about-descr">${movie.overview}</p>
-        <button type="button" class="btn">Add to my library</button>
+        <button type="button" class="up-btn">Add to my library</button>
     </div>
     `
 }
