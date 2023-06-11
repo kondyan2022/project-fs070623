94dd2d9be029cc8b04621ab776da463b5be1b606
Aleksandr Zhadanov <74437337+jadanov89@users.noreply.github.com>
2023-06-11 18:32:11 +0300

    1

--- src/js/hero.js
+++ src/js/hero.js
@@ -1,9 +1,15 @@
+<<<<<<< Updated upstream
 import TMDBApiService from './tmdb-api';
 
 import Swiper from 'swiper';
+=======
+
+// import Swiper from 'swiper';
+>>>>>>> Stashed changes
 
 const myApiService = new TMDBApiService();
 
+<<<<<<< Updated upstream
 const paretteContainer = document.querySelector('.swiper-wrapper');
 
 async function createMarkupFilms() {
@@ -36,9 +42,48 @@ async function createMarkupFilms() {
     console.error('Error fetching movies:', error);
   }
 }
+=======
+// const fetchTrendi = new TMDBApiService();
+
+// const emptyStar = `<svg class="star" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+// <path d="M16.875 7.3125H10.8281L9 1.6875L7.17188 7.3125H1.125L6.04688 10.6875L4.14844 16.3125L9 12.7969L13.8516 16.3125L11.9531 10.6875L16.875 7.3125Z" stroke="url(#paint0_linear_405_766)" stroke-linejoin="round"/>
+// <defs>
+// <linearGradient id="paint0_linear_405_766" x1="3.375" y1="2.625" x2="13.5" y2="16.5" gradientUnits="userSpaceOnUse">
+// <stop stop-color="#F84119"/>
+// <stop offset="1" stop-color="#F89F19" stop-opacity="0.68"/>
+// </linearGradient>
+// </defs>
+// </svg>`;
+
+// const fullStar = `<svg class="star" width="18" height="18" viewBox="0 0 18 18" fill="rgba(248, 65, 25, 1)" xmlns="http://www.w3.org/2000/svg">
+// <path d="M16.875 7.3125H10.8281L9 1.6875L7.17188 7.3125H1.125L6.04688 10.6875L4.14844 16.3125L9 12.7969L13.8516 16.3125L11.9531 10.6875L16.875 7.3125Z" stroke="url(#paint0_linear_405_766)" stroke-linejoin="round"/>
+// <defs>
+// <linearGradient id="paint0_linear_405_766" x1="3.375" y1="2.625" x2="13.5" y2="16.5" gradientUnits="userSpaceOnUse">
+// <stop stop-color="#F84119"/>
+// <stop offset="1" stop-color="#F89F19" stop-opacity="0.68"/>
+// </linearGradient>
+// </defs>
+// </svg>`;
+
+// const halfStar = `<svg class="star" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+// <path d="M16.875 7.3125H10.8281L9 1.6875L7.17188 7.3125H1.125L6.04688 10.6875L4.14844 16.3125L9 12.7969L13.8516 16.3125L11.9531 10.6875L16.875 7.3125Z" stroke="url(#paint0_linear_148_6991)" stroke-linejoin="round"/>
+// <path d="M9 1.6875V12.7969L4.14844 16.3125L6.04688 10.6875L1.125 7.3125H7.17188L9 1.6875Z" fill="url(#paint1_linear_148_6991)"/>
+// <defs>
+// <linearGradient id="paint0_linear_148_6991" x1="3.04877" y1="2.73251" x2="13.478" y2="16.7124" gradientUnits="userSpaceOnUse">
+// <stop stop-color="#F84119"/>
+// <stop offset="1" stop-color="#F89F19" stop-opacity="0.68"/>
+// </linearGradient>
+// <linearGradient id="paint1_linear_148_6991" x1="2.08688" y1="2.73251" x2="12.1506" y2="9.47748" gradientUnits="userSpaceOnUse">
+// <stop stop-color="#F84119"/>
+// <stop offset="1" stop-color="#F89F19" stop-opacity="0.68"/>
+// </linearGradient>
+// </defs>
+// </svg>`;
+>>>>>>> Stashed changes
 
 createMarkupFilms();
 
+<<<<<<< Updated upstream
 const swiper = new Swiper('.swiper-container', {
   loop: true,
   navigation: {
@@ -111,3 +156,54 @@ const swiper = new Swiper('.swiper-container', {
 
 
 // generateStarRating(0.5);
+=======
+// function getFiveStar(count) {
+//   const numStar = Math.round(count);
+//   return Array(5)
+//     .fill(0)
+//     .map((a, b) => {
+//       return numStar - 2 * (b + 1) >= 0
+//         ? '<div class="star"></div>'
+//         : numStar - 2 * (b + 1) + 1 == 0
+//         ? '<div class="star-half"></div>'
+//         : '<div class="star-outline"></div>';
+//     })
+//     .join('');
+// }
+
+// async function fetchStarRating() {
+//   try {
+//     const response = await fetch('fetchTrendi');
+//     const data = await response.json();
+//     const starRatingContainer = document.getElementById('starRating');
+//     starRatingContainer.innerHTML = getFiveStar(data.star);
+//   } catch (error) {
+//     console.error('Failed to fetch star rating:', error);
+//   }
+// }
+
+// fetchStarRating();
+
+// // function generateStarRating(rating) {
+// //   const starRatingContainer = document.querySelector(".starRating");
+// //   starRatingContainer.innerHTML = "";
+
+// //   const floorRating = Math.floor(rating);
+// //   const hasHalfStar = rating % 1 !== 0;
+
+// //   for (let i = 0; i < 5; i++) {
+// //     const star = document.createElement("span");
+// //     if (i < floorRating) {
+// //       star.innerHTML = fullStar;
+// //     } else if (i === floorRating && hasHalfStar) {
+// //       star.innerHTML = halfStar;
+// //     } else {
+// //       star.innerHTML = emptyStar;
+// //     }
+// //     starRatingContainer.appendChild(star);
+// //   }
+// // }
+
+
+// // generateStarRating(0.5);
+>>>>>>> Stashed changes
