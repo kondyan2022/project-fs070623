2739fe3726e5e0d531773ba2412af94b9ca5bfb6
Natalia-Havashi <natali.gavashi@gmail.com>
2023-06-11 08:41:40 +0300

    catalog

--- src/js/search.js
+++ src/js/search.js
@@ -1,13 +1,107 @@
+import getFilmCard from './film-card.js';  // розмітку HTML для відображення карточки фільму.
+import TMDBApiService from './tmdb-api.js' // пошук фільмів 
+import dataFormApi from '../testcatalog.json';
+const {results} = dataFormApi;
+import Notiflix from 'notiflix';
 import Pagination from 'tui-pagination';
-const container = document.getElementById('pagination');
+import 'tui-pagination/dist/tui-pagination.css';
+
+const ref = {
+    form: document.querySelector('.cataloge-search-form'),
+    input: document.querySelector('.cataloge-search-input'),
+    selectYear: document.getElementById('selectYear'),
+    deleteBtnInput: document.querySelector('.cataloge-btn-delete'),
+    searchBtn: document.getElementById('searchBtn'),
+    oopsNotFind: document.querySelector('.oops-not-find'),
+    choseMovie: document.querySelector('.choose-movie'),
+    }
+
+   const newElement = document.querySelector('.catalog-list-gallery');
+   const myService = new TMDBApiService();
+   let searchInput = '';
+   let searchYear = '';
+
+
+   ref.input.addEventListener('input', onInput);
+   function onInput (element) {
+        ref.deleteBtnInput.classList.remove('cataloge-btn-delete')
+        const valueInp = element.target.value.trim();
+        console.log(valueInp);
+        if(valueInp === '') {
+       Notiflix.Notify.info('');
+       ref.deleteBtnInput.classList.add('cataloge-bnt-delete');
+       ref.oopsNotFind.classList.add('oops-not-find-hide');
+       ref.choseMovie.classList.remove('choose-movie-hide');
+        }
+    }
+
+    ref.form.addEventListener('submit', onSubmit);
+
+
+
+function onSubmit(e) {
+    e.preventDefault();
+    searchInput = e.currentTarget.elements.SearchQuery.value.trim();
+    searchYear = e.currentTarget.elements.selectYear.value;
+
+    if(searchInput == '') {
+        ref.choseMovie.classList.remove('choose-movie-hide');
+        newElement.innerHTML = results
+        .map(a => getFilmCard(a, x =>
+            String(Math.round(x * 2) / 2)))
+            .join('')
+    } else {
+        const catalogURL = `https://api.themoviedb.org/3/search/movie?query=${searchInput}&include_adult=false&language=en-US&release_date.gte=${searchYear}&page=1`;
+
+        newElement.innerHTML = '';
+        ref.choseMovie.classList.add('choose-movie-hide');
+        ref.oopsNotFind.classList.add('oops-not-find-hide');
+        ref.deleteBtnInput.classList.add('cataloge-bnt-delete');
+
+        getFilmCard()
+        ref.form.reset()
+    }
+    }
+
+//Видаляємо з
+ref.deleteBtnInput.addEventListener('click', deleteValueInput);
+
+async function deleteValueInput (el) {
+el.preventDefault()
+ref.form.reset()
+ref.deleteBtnInput.classList.add('cataloge-btn-delete');
+ref.choseMovie.classList.add('choose-movie-hide');
+ref.oopsNotFind.classList.add('oops-not-find-hide');
+// const  catalogTrend = 'https://api.themoviedb.org/3/trending/all/week?page=1';
+newElement.innerHTML = results
+        .map(a => getFilmCard(a,x=>
+            String(Math.round(x * 2) / 2)))
+            .join('')
+try {
+    const response = await myService.fetchTrendingWeekMovies()
+    const trendingMovies = response.data.results;
+    displayMovies(trendingMovies);
+} catch (error){
+    console.error(error)
+}
+}
 
 
 
 
-const options = { // below default value of options
-     totalItems: 10,
+
+
+
+// ПАГІНАЦІЯ
+const refs = {
+    paginationElem: document.querySelector('.tui-pagination'),
+}
+
+
+const options = { 
+     totalItems: 1000,
      itemsPerPage: 20,
-     visiblePages: 10,
+     visiblePages: 6,
      page: 1,
      centerAlign: false,
      firstItemClassName: 'tui-first-child',
@@ -28,9 +122,21 @@ const options = { // below default value of options
                  '<span class="tui-ico-ellip">...</span>' +
              '</a>'
      }
-};
-const pagination = new Pagination(container, options);
+}
 
+
+const pagination = new Pagination(refs.paginationElem, options);
 console.log('pagination:', pagination)
+let pageForPagination = 1;
 
+console.log('pageForPagination:', pageForPagination)
+
+pagination.on('afterMove', (event) => {
+    const currentPage = event.page;
+   pageForPagination = currentPage;  
+  console.log(currentPage);
+
+  
+});
+console.log('pageForPagination:', pageForPagination)
 
