299d7dc7fac04f9671a123227bb7f58a5a8556d5
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-06-12 16:25:03 +0200

    Update liblist.js

--- src/js/liblist.js
+++ src/js/liblist.js
@@ -7,10 +7,10 @@ import getFiveStar from './fivezerostar.js';
 import '../sass/_fivestar.scss';
 
 const refs = {
-  listCards: document.querySelector('.js-liblist'),
-  selectWrapper: document.querySelector('#library-filter'),
-  wrapperForMessage: document.querySelector('.js-lib-content-wrap'),
-  btnLoadMore: document.querySelector('.js-load-more'),
+    listCards: document.querySelector('.js-liblist'),
+    selectWrapper: document.querySelector('#library-filter'),
+    wrapperForMessage: document.querySelector('.js-lib-content-wrap'),
+    btnLoadMore: document.querySelector('.js-load-more'),
 };
 
 const arrayGenres = getGenres(); // console.log(arrayGenres)
@@ -30,7 +30,7 @@ let gotMovies = 9; //getMovies.length;
  * @returns markup
  */
 function createMarkupInSelect(item) {
-  return `
+    return `
     <option class="lib-option" value="${item.id}">${item.name}</option>
     `;
 }
@@ -39,11 +39,11 @@ function createMarkupInSelect(item) {
  * function by render genre of movie to select
  */
 function renderMarkupInSelect() {
-  const gotGenre = arrayGenres
-    .map(genre => createMarkupInSelect(genre))
-    .join('');
-  // refs.selectWrapper.innerHTML = gotGenre;
-  refs.selectWrapper.insertAdjacentHTML('beforeend', gotGenre);
+    const gotGenre = arrayGenres
+        .map(genre => createMarkupInSelect(genre))
+        .join('');
+    // refs.selectWrapper.innerHTML = gotGenre;
+    refs.selectWrapper.insertAdjacentHTML('beforeend', gotGenre);
 }
 
 /**-----------------------------------------------------------------------
@@ -51,74 +51,74 @@ function renderMarkupInSelect() {
  * @param {*array} list
  */
 function renderMovieInCards(list) {
-  refs.listCards.innerHTML = list
-    .map(film => getFilmCard(film, getFiveStar))
-    .join('');
+    refs.listCards.innerHTML = list
+        .map(film => getFilmCard(film, getFiveStar))
+        .join('');
 }
 
 /**-----------------------------------------------------------------------
  * function to show cards and select
  */
-function showContent() {
-  if (getMovies.length > 0) {
-    renderMarkupInSelect();
+// function showContent() {
+//   if (getMovies.length > 0) {
+//     renderMarkupInSelect();
 
-    renderMovieInCards(getMovies); //???
+//     renderMovieInCards(getMovies); //???
 
-    //use older pagination
+//     //use older pagination
 
-    // const startMovies = getMovies.slice(0, perPageMovies);
-    // renderMovieInCards(startMovies);
+//     // const startMovies = getMovies.slice(0, perPageMovies);
+//     // renderMovieInCards(startMovies);
 
-    // if (getMovies.length <= perPageMovies) {
-    //     refs.btnLoadMore.style.display = 'none';
-    // } else {
-    //     refs.btnLoadMore.style.display = 'block';
-    //     // gotMovies = perPageMovies;
-    // }
+//     // if (getMovies.length <= perPageMovies) {
+//     //     refs.btnLoadMore.style.display = 'none';
+//     // } else {
+//     //     refs.btnLoadMore.style.display = 'block';
+//     //     // gotMovies = perPageMovies;
+//     // }
 
-    //----------test use pagination----------
-    // const { markupPage, isMore } = paginationSavedCards(getMovies, perPageMovies);
-    // // refs.listCards.innerHtml = markupPage;
+//     //----------test use pagination----------
+//     // const { markupPage, isMore } = paginationSavedCards(getMovies, perPageMovies);
+//     // // refs.listCards.innerHtml = markupPage;
 
-    // renderMovieInCards(markupPage)
+//     // renderMovieInCards(markupPage)
 
-    // if (isMore) {
-    //     refs.btnLoadMore.style.display = 'block';
-    // } else {
-    //     refs.btnLoadMore.style.display = 'none';
-    // }
-    //---------------------------------------
-  } else {
-    refs.selectWrapper.style.visibility = 'hidden';
-    refs.wrapperForMessage.innerHTML =
-      '<p class="lib-error">OOPS... We are very sorry! You dont have any movies at your library.</p >  <a href="./catalog.html" class="lib-btn-search-movie">Search movie</a>';
-    refs.btnLoadMore.style.display = 'none';
-  }
-}
+//     // if (isMore) {
+//     //     refs.btnLoadMore.style.display = 'block';
+//     // } else {
+//     //     refs.btnLoadMore.style.display = 'none';
+//     // }
+//     //---------------------------------------
+//   } else {
+//     refs.selectWrapper.style.visibility = 'hidden';
+//     refs.wrapperForMessage.innerHTML =
+//       '<p class="lib-error">OOPS... We are very sorry! You dont have any movies at your library.</p >  <a href="./catalog.html" class="lib-btn-search-movie">Search movie</a>';
+//     refs.btnLoadMore.style.display = 'none';
+//   }
+// }
 
 /**-----------------------------------------------------------------------
  * adds handler on the select
  */
 // refs.selectWrapper.addEventListener('change', onSelect);
 
-function onSelect() {
-  const selectedGenre = Number(refs.selectWrapper.value);
+// function onSelect() {
+//   const selectedGenre = Number(refs.selectWrapper.value);
 
-  const filteredGotMovies = getMovies.filter(({ genre_ids }) =>
-    genre_ids.includes(selectedGenre)
-  );
-  const optionValue = refs.selectWrapper.value;
-  console.log(optionValue);
+//   const filteredGotMovies = getMovies.filter(({ genre_ids }) =>
+//     genre_ids.includes(selectedGenre)
+//   );
+//   const optionValue = refs.selectWrapper.value;
+//   console.log(optionValue);
 
-  if (optionValue === 'all') {
-    renderMovieInCards(getMovies);
-  } else if (filteredGotMovies.length > 0) {
-    renderMovieInCards(filteredGotMovies);
-  } else {
-    showMessage();
-  }
-}
+//   if (optionValue === 'all') {
+//     renderMovieInCards(getMovies);
+//   } else if (filteredGotMovies.length > 0) {
+//     renderMovieInCards(filteredGotMovies);
+//   } else {
+//     showMessage();
+//   }
+// }
 
 // calling a function
 // showContent();
@@ -127,12 +127,12 @@ function onSelect() {
  * function to show the message and button 'Search movie'
  */
 function showMessage() {
-  // refs.listCards.innerHTML = '';
-  // refs.selectWrapper.style.visibility = 'hidden';
-  const messageMarkup =
-    '<p class="lib-error">No movies in the selected genre!</p> <a href="./catalog.html" class="lib-btn-search-movie">Search movie</a>';
-  refs.wrapperForMessage.innerHTML = messageMarkup;
-  refs.btnLoadMore.style.display = 'none';
+    // refs.listCards.innerHTML = '';
+    // refs.selectWrapper.style.visibility = 'hidden';
+    const messageMarkup =
+        '<p class="lib-error">No movies in the selected genre!</p> <a href="./catalog.html" class="lib-btn-search-movie">Search movie</a>';
+    refs.wrapperForMessage.innerHTML = messageMarkup;
+    refs.btnLoadMore.style.display = 'none';
 }
 
 //---------------------load more------------------------------------------
@@ -155,28 +155,29 @@ function showMessage() {
 //     refs.listCards.insertAdjacentHTML('beforeend', loadNext);
 // }
 //------------------------------------------------------------------------
-
+renderMarkupInSelect();
 // library slim-select
 
+
 let select = new SlimSelect({
-  select: refs.selectWrapper,
-  events: {
-    afterChange: x => {
-      onNewSelect(x);
+    select: refs.selectWrapper,
+    events: {
+        afterChange: x => {
+            onNewSelect(x);
+        },
+        // beforeOpen: () => {
+        //     console.log('before open')
+        // },
+        // afterOpen: () => {
+        //     console.log('after open')
+        // },
+        // beforeClose: () => {
+        //     console.log('before close')
+        // },
+        // afterClose: () => {
+        //     console.log('after close')
+        // },
     },
-    // beforeOpen: () => {
-    //     console.log('before open')
-    // },
-    // afterOpen: () => {
-    //     console.log('after open')
-    // },
-    // beforeClose: () => {
-    //     console.log('before close')
-    // },
-    // afterClose: () => {
-    //     console.log('after close')
-    // },
-  },
 });
 
 //---------------------PAGINATION-------------------------------------
@@ -184,11 +185,12 @@ let select = new SlimSelect({
 refs.btnLoadMore.addEventListener('click', onLoadMore);
 
 function onLoadMore() {
-  if (select.getSelected()[0] === 'all') {
-    createPaginationMarkUp(getMovies, currentCard);
-  } else {
-    createPaginationMarkUp(arrayFilter, currentCard);
-  } //   createPaginationMarkUp(array, lastCard);
+    console.log(select.getSelected()[0], currentCard);
+    if (select.getSelected()[0] === 'all') {
+        createPaginationMarkUp(getMovies, currentCard);
+    } else {
+        createPaginationMarkUp(arrayFilter, currentCard);
+    } //   createPaginationMarkUp(array, lastCard);
 }
 
 // function paginationSavedCards(array, quantityCard) {
@@ -208,43 +210,43 @@ function onLoadMore() {
 // }
 
 function paginationSavedCards(array, firstPosition, quantityCard) {
-  const sliceMovies = firstPosition + quantityCard;
-  const shownMovies = array.slice(firstPosition, quantityCard);
-  currentCard = firstPosition + array.length;
-  const isMore = array.length > sliceMovies; //false ar true
-  console.log(sliceMovies, array.length);
-  return {
-    listHTML: array
-      .map(film => {
-        return getFilmCard(film, getFiveStar);
-      })
-      .join(''),
-    hasMore: isMore,
-  };
+    //   const sliceMovies = firstPosition + quantityCard;
+    const shownMovies = array.slice(firstPosition, firstPosition + quantityCard);
+    console.log(shownMovies);
+    currentCard = firstPosition + shownMovies.length;
+    const isMore = array.length > currentCard; //false ar true
+    console.log(currentCard, array.length);
+    return {
+        listHTML: shownMovies
+            .map(film => {
+                return getFilmCard(film, getFiveStar);
+            })
+            .join(''),
+        hasMore: isMore,
+    };
 }
 
 function createPaginationMarkUp(array, lastCard) {
-  const { listHTML, hasMore } = paginationSavedCards(array, lastCard, 9);
-  refs.listCards.insertAdjacentHTML('beforeend', listHTML);
+    const { listHTML, hasMore } = paginationSavedCards(array, lastCard, 9);
+    refs.listCards.insertAdjacentHTML('beforeend', listHTML);
 
-  refs.btnLoadMore.style.display = hasMore ? 'block' : 'none';
+    refs.btnLoadMore.style.display = hasMore ? 'block' : 'none';
 }
 
 function onNewSelect(genreId) {
-  console.log(genreId);
-
-  refs.listCards.innerHTML = '';
-  if (String(genreId[0].value) === 'all') {
-    arrayFilter = [];
-    console.log('all ok');
-    createPaginationMarkUp(getMovies, currentCard);
-  } else {
-    arrayFilter = getMovies.filter(({ genre_ids }) =>
-      genre_ids.includes(Number(genreId[0].value))
-    );
-    createPaginationMarkUp(arrayFilter, currentCard);
-  }
+    console.log(genreId);
+    currentCard = 0;
+    refs.listCards.innerHTML = '';
+    if (String(genreId[0].value) === 'all') {
+        arrayFilter = [];
+        console.log('all ok');
+        createPaginationMarkUp(getMovies, currentCard);
+    } else {
+        arrayFilter = getMovies.filter(({ genre_ids }) =>
+            genre_ids.includes(Number(genreId[0].value))
+        );
+        createPaginationMarkUp(arrayFilter, currentCard);
+    }
 }
 
-currentCard = getMovies.length > 9 ? 9 : getMovies.length;
-onNewSelect([{ value: 'all' }]);
+select.setSelected('all');
