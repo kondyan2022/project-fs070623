8e447f7d52050d2ca195bb8ca29f1939c36abb5a
Inna Shkilna <shkinai.1995@gmail.com>
2023-06-12 21:58:14 +0300

    edit css, js

--- src/js/toggle-switch.js
+++ src/js/toggle-switch.js
@@ -2,21 +2,14 @@ const refs = {
   toggleSwitch: document.querySelector('.toggle-switch'),
   checkbox: document.querySelector('.checkbox'),
   body: document.querySelector('body'),
-  logo: document.querySelector('.logo-title'),
-  navigation: document.querySelector('.header-nav'),
-  slider: document.querySelector('.slider'),
 };
 
 function setTheme(theme) {
   if (theme === 'light') {
     refs.body.classList.add('js-light-theme');
-    refs.logo.classList.add('js-light-theme-logo');
-    refs.navigation.classList.add('js-light-theme-text');
     refs.checkbox.checked = true;
   } else {
     refs.body.classList.remove('js-light-theme');
-    refs.logo.classList.remove('js-light-theme-logo');
-    refs.navigation.classList.remove('js-light-theme-text');
     refs.checkbox.checked = false;
   }
 }
@@ -36,4 +29,4 @@ if (savedTheme) {
   setTheme(savedTheme);
 }
 
-refs.toggleSwitch.addEventListener('click', handleToggle);
+refs.toggleSwitch.addEventListener('change', handleToggle);
