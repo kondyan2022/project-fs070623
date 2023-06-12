7d8f38455d663f51e467f13b944d4fed88387d0e
Inna Shkilna <shkinai.1995@gmail.com>
2023-06-13 01:10:05 +0300

    transition slider

--- src/js/toggle-switch.js
+++ src/js/toggle-switch.js
@@ -2,12 +2,17 @@ const refs = {
   toggleSwitch: document.querySelector('.toggle-switch'),
   checkbox: document.querySelector('.checkbox'),
   body: document.querySelector('body'),
+  slider: document.querySelector('.slider'),
 };
 
 function setTheme(theme) {
   if (theme === 'light') {
     refs.body.classList.add('js-light-theme');
     refs.checkbox.checked = true;
+    setTimeout(() => {
+      refs.slider.style.transition =
+        'transform 400ms cubic-bezier(0.4, 0, 0.2, 1)';
+    }, 500);
   } else {
     refs.body.classList.remove('js-light-theme');
     refs.checkbox.checked = false;
@@ -26,7 +31,11 @@ function handleToggle() {
 
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme) {
+  // Set initial state based on saved theme
   setTheme(savedTheme);
+} else {
+  // Set initial state based on default theme
+  setTheme('dark');
 }
 
 refs.toggleSwitch.addEventListener('change', handleToggle);
