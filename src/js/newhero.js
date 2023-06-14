43514d9af394e1a73673385f28e3589bf2f764ef
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-06-14 15:32:38 +0200

    +

--- src/js/newhero.js
+++ src/js/newhero.js
@@ -1,35 +1,57 @@
 import TMDBApiService from './tmdb-api';
+// import { modalController } from './modal-trailer';
+import getFiveStar from './fivezerostar.js';
+import '../sass/_fivestar.scss';
+// import Swiper from 'swiper';
+// import 'swiper/swiper.min.css';
+// import 'swiper/swiper.css';
+
+// //=================swiper
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'horizontal',
+//     loop: true
+// });
+
+console.log(swiper)
 
 function createMarkup(movie) {
     return `
-        <div class="newhero-content-wrapper">
-            <div class="newhero-thumb">
-                <img
-                    srcset="
-                        https://image.tmdb.org/t/p/w300/${movie.backdrop_path}      300w,
-                        https://image.tmdb.org/t/p/w780/${movie.backdrop_path}      780w,
-                        https://image.tmdb.org/t/p/w1280/${movie.backdrop_path}      1280w,
-                        https://image.tmdb.org/t/p/original/${movie.backdrop_path}  3840w
-                    "
-                    sizes="(min-width: 320px) 498px,
-                            (min-width: 768px) 704px,
-                            (min-width: 1280px) 805px,
-                            100vw"
-                    src="https://image.tmdb.org/t/p/w300/${movie.backdrop_path}"
-                    alt="${movie.title}"
-                    class="newhero-image"
-                    width="1280"
-                    height="720"
-                />
-            </div>
-            <h2 class="newhero-movie-title">${movie.title}</h2>
-            <div>STARS</div>
-            <p class="newhero-about-descr">${movie.overview}</p>
-            <div class="newhero-wrap-btn">
-                <button type="button" class="newhero-btn js-newhero-open-modal-tr">Watch trailer</button>
-            </div>
-            <div class="newhero-wrap-btn">
-                <button type="button" class="newhero-btn js-newhero-open-mod-det">More details</button>
+        <div class="swiper">
+            <div class="swiper-wrapper newhero-content-wrapper">
+                <div class="swiper-slide">
+                    <div class="newhero-thumb">
+                        <img
+                            srcset="
+                                https://image.tmdb.org/t/p/w300/${movie.backdrop_path}      300w,
+                                https://image.tmdb.org/t/p/w780/${movie.backdrop_path}      780w,
+                                https://image.tmdb.org/t/p/w1280/${movie.backdrop_path}      1280w,
+                                https://image.tmdb.org/t/p/original/${movie.backdrop_path}  3840w
+                            "
+                            sizes="(min-width: 320px) 320px,
+                                    (min-width: 768px) 768px,
+                                    (min-width: 1280px) 802px,
+                                    100vw"
+                            src="https://image.tmdb.org/t/p/w300/${movie.backdrop_path}"
+                            alt="${movie.title}"
+                            class="newhero-image"
+                            width="1280"
+                            height="720"
+                        />
+                    </div>
+
+                    <div class="newhero-movie-inform-wrap">
+                        <h1 class="newhero-movie-title">${movie.title}</h1>
+                        <div class="newhero-stars">${getFiveStar(movie.vote_average)}</div>
+                        <p class="newhero-about-descr">${movie.overview}</p>
+                        <div class="newhero-wrap-btn newh-wrap-trailer">
+                            <button type="button" class="newhero-btn js-newhero-open-modal-tr">Watch trailer</button>
+                        </div>
+                        <div class="newhero-wrap-btn">
+                            <button type="button" class="newhero-btn js-newhero-open-mod-det">More details</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     `
@@ -68,30 +90,24 @@ serviceTrendingDaysMovies
             wrapperForRender.addEventListener('click', (e) => {
                 console.log(e.currentTarget, e.target)
                 console.log(e.currentTarget)
-                if (e.target === document.querySelector('.js-newhero-open-modal-tr')) {
-                    console.log("button TRAILER")
-                    /// open modal with trailer
-                    wrapperContent.addEventListener('click', hendlerOpenModalWindow);
-                } else if (e.target === document.querySelector('.js-newhero-open-mod-det')) {
+                if (e.target === document.querySelector('.js-newhero-open-mod-det')) {
                     /// open modal with details 
                     console.log('button DETAILS')
-                    ///???????????????????????????
                     wrapperContent.addEventListener('click', () => {
                         console.log(evt.target.getAttribute('film-id'), 'Це id фільму')
                     });
-                    //?????????????????????????????
                 }
+                // else if (e.target === document.querySelector('.js-newhero-open-modal-tr')) {
+                //     wrapperContent.addEventListener('click', hendlerOpenModalWindow);
+                // }
             })
             return
         }
-        // else if (arrayResults.length === 0) {
-        //     wrapperForRender.innerHTML = '<p class="upcoming-error">OOPS... We are very sorry! Upcoming this month not found.</p>'
-        // }
     })
     .catch((e) => console.error(e));
 
 function renderToMarkup(array) {
-    const randomIndex = getRandomIndex();
+    // const randomIndex = getRandomIndex();
     const randomMovie = array
         .filter(movie => movie.backdrop_path !== null)
         .map(movie => {
@@ -100,23 +116,27 @@ function renderToMarkup(array) {
                 id: movie.id,
                 markup: movieMarkup
             }
-        })[randomIndex];
+        }); //[randomIndex]
 
     console.log(randomMovie)
 
     const { id, markup } = randomMovie;
     // wrapperForRender.innerHTML = markup;
-    // if (arrayResults.length > 0) {
-    //     wrapperForRender.innerHTML = markup;
-    //     return
-    // }
 
     setTimeout(() => {
         wrapperForRender.innerHTML = markup;
-        console.log('timeout')
+        console.log('timeout');
+        // modalController({
+        //     modal: '.modal1',
+        //     btnOpen: '.js-newhero-open-modal-tr',
+        //     btnClose: '.modalclose',
+        // });
+
     }, 3000);
 }
 
-function getRandomIndex() {
-    return Math.floor(Math.random() * 15);
-}
+// function getRandomIndex() {
+//     return Math.floor(Math.random() * 15);
+// }
+
+
