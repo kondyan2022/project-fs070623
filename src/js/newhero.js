84797e0c4b149fe233b7ba94ae44146debaf6a96
InnaAnatoliivna <120109442+InnaAnatoliivna@users.noreply.github.com>
2023-06-18 15:16:10 +0200

    add swiper

--- src/js/newhero.js
+++ src/js/newhero.js
@@ -3,9 +3,16 @@ import { modalController } from './modal-trailer';
 import getFiveStar from './fivezerostar.js';
 import { openModalCard } from './modal-poster';
 import '../sass/_fivestar.scss';
+//swiper------>
 import Swiper from 'swiper';
+import Swiper, { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper';
 import 'swiper/swiper.min.css';
-// import 'swiper/swiper.css';
+import 'swiper/modules/autoplay/autoplay.min.css';
+Swiper.use([Navigation, Pagination, Scrollbar, Autoplay]);
+// import 'swiper/modules/navigation/navigation.min.css';
+// import 'swiper/modules/pagination/pagination.min.css';
+// import 'swiper/modules/scrollbar/scrollbar.min.css';
+
 
 let numberLetters = 200;
 
@@ -37,24 +44,24 @@ function createMarkup(movie) {
                     src="https://image.tmdb.org/t/p/w300/${movie.backdrop_path
     }"
                     alt="${movie.title}"
-                    class="newhero-image"
+                    class="newhero-image swiper-img"
                     width="1280"
-                      height="720"
+                    height="720"
                 />
             </div>
 
-            <div class="newhero-movie-inform-wrap">
+            <div class="newhero-inform-wrap">
                 <h1 class="newhero-movie-title">${movie.title.substring(0, 25)}</h1>
                 <div class="newhero-stars">${getFiveStar(
       movie.vote_average
     )}</div>
-                <p class="newhero-about-descr">${movie.overview.substring(0, numberLetters)}...</p>
+                <p class="newhero-about">${movie.overview.substring(0, numberLetters)}...</p>
                 <div class="newh-wrap-buttons">
-                    <div class="newhero-wrap-btn-api-one newh-wrap-trailer">
-                        <button type="button" class="newhero-btn-api-one js-newhero-open-modal-tr">Watch trailer</button>
+                    <div class="newh-wrap-trailer">
+                        <button type="button" class="newhero-modal-trailer js-newhero-modal-trailer">Watch trailer</button>
                     </div>
-                    <div class="newhero-wrap-btn-api-two newh-wrap-detail">
-                        <button type="button" class="newhero-btn-api-two js-newhero-open-mod-det">More details</button>
+                    <div class="newh-wrap-detail">
+                        <button type="button" class="newhero-modal-detail .js-newhero-modal-detail">More details</button>
                     </div>
                   </div>
               </div>
@@ -83,36 +90,21 @@ serviceTrendingDaysMovies
       return;
     }
   })
-  .then(resp => {
+  .then(() => {
     if (shouldInitSwiper) {
       setTimeout(() => {
         const swiper = new Swiper('.swiper', {
           autoplay: {
-            delay: 1000,
+            delay: 5000,
             disableOnInteraction: false,
           },
-          // navigation: {
-          //   nextEl: '.swiper-button-next',
-          //   prevEl: '.swiper-button-prev',
-          // },
           slidesPerView: 1,
           spaceBetween: 10,
-          // simulateTouch: false,
           direction: 'horizontal',
           loop: true,
-          effect: 'fade',
-          fadeEffect: {
-            crossFade: true,
-          },
-          // observer: true
+          observer: true,
         });
-
-        swiper.init();
-        // swiper.autoplay.delay = 2000;
-        // swiper.autoplay.start();
-        // swiper.autoplay.start();
-        console.log(swiper, 'swipeeer');
-      }, 1000);
+      }, 0);
     }
   })
   .catch(e => console.error(e));
@@ -122,14 +114,13 @@ serviceTrendingDaysMovies
 wrapperForRender.addEventListener('click', onBtnDetails)
 
 function onBtnDetails(evt) {
-  if (evt.target === document.querySelector('.js-newhero-open-mod-det')) {
+  if (evt.target === document.querySelector('.js-newhero-modal-detail')) {
     const el = evt.target.closest('[film-id]');// console.log('found', el);
     if (el) {// console.log('button DETAILS');
       openModalCard(el.getAttribute('film-id'));
     }
   }
 }
-//=================swiper======================================
 
 function renderToMarkup(array) {
   const slides = array
@@ -140,36 +131,8 @@ function renderToMarkup(array) {
     wrapperForRender.innerHTML = slides;
     modalController({
       modal: '.modal1',
-      btnOpen: '.js-newhero-open-modal-tr',
+      btnOpen: '.js-newhero-modal-trailer',
       btnClose: '.modalclose',
     });
   }, 1000);
 }
-
-//==========================================================================
-//---------------------------------changes this render function without slider
-// function renderToMarkup(array) {
-//   const randomIndex = getRandomIndex();
-//   const randomMovie = array
-//     .filter(movie => movie.backdrop_path !== null)
-//     .map(movie => {
-//       const movieMarkup = createMarkup(movie);
-//       return {
-//         markup: movieMarkup,
-//       };
-//     })[randomIndex];
-//   const { markup } = randomMovie;
-//   setTimeout(() => {
-//     wrapperForRender.innerHTML = markup;
-
-//     modalController({
-//       modal: '.modal1',
-//       btnOpen: '.js-newhero-open-modal-tr',
-//       btnClose: '.modal__close',
-//     });
-//   }, 3000);
-// }
-// function getRandomIndex() {
-//   return Math.floor(Math.random() * 15);
-// }
-
